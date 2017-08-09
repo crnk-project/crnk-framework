0e8e44ab7b5a9e4483cf0113639cb6b115ed8217
Remo Meier <remo.meier@adnovum.ch>
2017-08-09 18:00:48 +0200

    testing

--- crnk-client-ngrx/karma.conf.js
+++ crnk-client-ngrx/karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function (config) {
 	config.set({
 		basePath: '',
-		frameworks: ['jasmine'],
+		frameworks: ["jasmine", "karma-typescript"],
 
 		plugins: [
 			require('karma-jasmine'),
@@ -13,33 +13,67 @@ module.exports = function (config) {
 			require('karma-firefox-launcher'),
 			require('karma-jasmine-html-reporter'),
 			require('karma-coverage-istanbul-reporter'),
-			require('karma-junit-reporter')
+			require('karma-junit-reporter'),
+			require('karma-typescript')
+			//require('karma-typescript-preprocessor')
 		],
 		client: {
 			// leave Jasmine Spec Runner output visible in browser
 			clearContext: false
 		},
 		files: [
-			{pattern: './src/test/typescript/test.bundle.ts', watched: false}
+			{ pattern: "src/main/typescript/**/*.ts" },
+			{ pattern: "src/test/typescript/**/*.ts" }
+
+			// {pattern: './src/test/typescript/test.bundle.ts', watched: false}
 		],
 		preprocessors: {
-			//'./src/test.ts': ['@angular/cli']
+			"src/main/typescript/**/*.ts": ["karma-typescript"],
+			"test/test/typescript/**/*.ts": ["karma-typescript"]
+
+			// "**/*.ts": ["karma-typescript"],
+			// '**/*.ts': ['typescript']
 		},
 		mime: {
 			'text/x-typescript': ['ts', 'tsx']
 		},
+		karmaTypescriptConfig: {
+			bundlerOptions: {
+				entrypoints: /\.spec\.ts$/,
+				transforms: [
+					require('karma-typescript-angular2-transform'),
+					require("karma-typescript-es6-transform")()
+				]
+			},
+
+
+			compilerOptions: {
+				lib: ['ES2015', 'DOM'],
+				module: "commonjs",
+				emitDecoratorMetadata: true,
+				experimentalDecorators: true,
+				sourceMap: true,
+				target: "ES5"
+			}
+		},
+
+
+
 		coverageIstanbulReporter: {
 			reports: ['html', 'lcovonly'],
 			fixWebpackSourcePaths: true
 		},
-		reporters: ['progress', 'junit'].concat(
-			config.angularCli && config.angularCli.codeCoverage ? ['coverage-istanbul'] : ['kjhtml']),
+		// reporters: ['progress', 'junit'].concat(
+		//	config.angularCli && config.angularCli.codeCoverage ? ['coverage-istanbul'] : ['kjhtml']),
+
+		reporters: ['progress', 'karma-typescript'],
+
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
 		browsers: ['Firefox'],
-		singleRun: false,
+		singleRun: true,
 		junitReporter: {
 			outputDir: 'build/test/log/karma',
 			outputFile: 'unit.xml',
--- crnk-client-ngrx/src/test/typescript/operations.spec.ts
+++ crnk-client-ngrx/src/test/typescript/operations.spec.ts
@@ -1,245 +1,43 @@
-import {
-	async,
-	inject,
-	fakeAsync,
-	tick,
-	TestBed
-} from '@angular/core/testing';
+import { TestBed, inject, async, tick, fakeAsync } from '@angular/core/testing';
 
-import { Store, StoreModule } from '@ngrx/store';
+//import { SampleModule, SampleService } from "./";
 
-import {
-	EffectsTestingModule,
-	EffectsRunner
-} from '@ngrx/effects/testing';
-
-import { NgrxJsonApi } from '../src/api';
-import { NgrxJsonApiService } from '../src/services';
-import { NgrxJsonApiSelectors } from '../src/selectors';
-import { NgrxJsonApiEffects } from '../src/effects';
-
-import {
-	NGRX_JSON_API_CONFIG,
-	apiFactory,
-	selectorsFactory,
-} from '../src/module';
-
-import {
-	initialNgrxJsonApiState,
-	NgrxJsonApiStoreReducer,
-} from '../src/reducers';
-
-import {
-	ApiPostInitAction,
-	ApiPostSuccessAction,
-	ApiPostFailAction,
-	ApiPatchInitAction,
-	ApiPatchSuccessAction,
-	ApiPatchFailAction,
-	ApiGetInitAction,
-	ApiGetSuccessAction,
-	ApiGetFailAction,
-	ApiDeleteInitAction,
-	ApiDeleteSuccessAction,
-	ApiDeleteFailAction,
-	LocalQueryInitAction,
-	LocalQuerySuccessAction,
-	LocalQueryFailAction
-} from '../src/actions';
-
-import {
-	testPayload,
-	resourceDefinitions
-} from './test_utils';
-
-import { updateStoreDataFromPayload } from '../src/utils';
-
-
-import {
-	MOCK_JSON_API_PROVIDERS,
-	MOCK_NGRX_EFFECTS_PROVIDERS
-} from './testing';
-
-describe('NgrxJsonApiEffects', () => {
-	let runner: EffectsRunner;
-	let effects;
-
+describe('SampleTest', () => {
 	beforeEach(() => {
-		let store = {
-			api: Object.assign({}, initialNgrxJsonApiState, {
-				data: updateStoreDataFromPayload({}, testPayload),
-			}, )
-		};
 		TestBed.configureTestingModule({
-			imports: [
-				EffectsTestingModule,
-				StoreModule.provideStore({ api: NgrxJsonApiStoreReducer }, store),
-			],
 			providers: [
-				...MOCK_JSON_API_PROVIDERS,
-				...MOCK_NGRX_EFFECTS_PROVIDERS,
+				/*
 				{
-					provide: NgrxJsonApiSelectors,
-					useFactory: selectorsFactory,
-					deps: [NGRX_JSON_API_CONFIG]
-				},
-				{
-					provide: NGRX_JSON_API_CONFIG,
-					useValue: {
-						storeLocation: 'api',
-						resourceDefinitions: resourceDefinitions
+					provide: SampleService,
+					deps: [],
+					useFactory: () => {
+						return new SampleService()
 					}
 				}
+				*/
+			],
+			imports: [
+				// SampleModule
 			]
 		})
-	});
+	})
 
-	beforeEach(inject([EffectsRunner, NgrxJsonApiEffects],
-		(_runner, _effects) => {
-			runner = _runner;
-			effects = _effects;
-		}
-	));
+	it('should expect', () => {
+		expect(true).toBeTruthy()
+	})
 
-	let successPayload = {
-		jsonApiData: {
-			data: {
-				type: 'SUCCESS'
-			}
-		},
-		query: {
-			type: 'SUCCESS'
-		}
-	};
-	let failPayload = {
-		jsonApiData: {
-			data: {
-				type: 'FAIL'
-			}
-		},
-		query: {
-			type: 'FAIL'
-		}
-	};
-	let successQuery = {
-		query: {
-			type: 'SUCCESS'
-		}
-	};
-	let failQuery = {
-		query: {
-			type: 'FAIL'
-		}
-	};
+	it('should expect async', async(() => {
+		setTimeout(() => {
+			expect(true).toBeTruthy()
+		}, 500)
+	}))
 
-	it('should respond to successfull CREATE_INIT action', () => {
-		let res;
-		runner.queue(new ApiPostInitAction(successPayload));
-		effects.createResource$.subscribe(result => {
-			res = result;
-			expect(result).toEqual(
-				new ApiPostSuccessAction(successPayload));
-		});
-		expect(res).toBeDefined();
-	});
+	it('should expect fakeAsync', fakeAsync(() => {
+		tick()
+		expect(true).toBeTruthy()
+	}))
 
-	it('should respond to failed CREATE_INIT action', () => {
-		let res;
-		runner.queue(new ApiPostInitAction(failPayload));
-		effects.createResource$.subscribe(result => {
-			res = result;
-			expect(result).toEqual(
-				new ApiPostFailAction(failPayload));
-		});
-		expect(res).toBeDefined();
-	});
-
-	it('should respond to successfull UPDATE_INIT action', () => {
-		let res;
-		runner.queue(new ApiPatchInitAction(successPayload));
-		effects.updateResource$.subscribe(result => {
-			res = result;
-			expect(result).toEqual(
-				new ApiPatchSuccessAction(successPayload));
-		});
-		expect(res).toBeDefined();
-	});
-
-	it('should respond to failed UPDATE_INIT action', () => {
-		let res;
-		runner.queue(new ApiPatchInitAction(failPayload));
-		effects.updateResource$.subscribe(result => {
-			res = result;
-			expect(result).toEqual(
-				new ApiPatchFailAction(failPayload));
-		});
-		expect(res).toBeDefined();
-	});
-
-	it('should respond to successfull READ_INIT action', () => {
-		let res;
-		runner.queue(new ApiGetInitAction(successQuery.query));
-		effects.readResource$.subscribe(result => {
-			res = result;
-			expect(result).toEqual(
-				new ApiGetSuccessAction({
-					jsonApiData: result.payload.jsonApiData,
-					query: successQuery.query
-				}));
-		});
-		expect(res).toBeDefined();
-	});
-
-	it('should respond to failed READ_INIT action', () => {
-		let res;
-		runner.queue(new ApiGetInitAction(failQuery.query));
-		effects.readResource$.subscribe(result => {
-			res = result;
-			expect(result).toEqual(
-				new ApiGetFailAction(failQuery));
-		});
-		expect(res).toBeDefined();
-	});
-
-	it('should respond to successfull DELETE_INIT action', () => {
-		let res;
-		runner.queue(new ApiDeleteInitAction(successQuery));
-		effects.deleteResource$.subscribe(result => {
-			res = result;
-			expect(result).toEqual(
-				new ApiDeleteSuccessAction(Object.assign({}, successQuery, { jsonApiData: null }));
-		});
-		expect(res).toBeDefined();
-	});
-
-	it('should respond to failed DELETE_INIT action', () => {
-		let res;
-		runner.queue(new ApiDeleteInitAction(failQuery));
-		effects.deleteResource$.subscribe(result => {
-			res = result;
-			expect(result).toEqual(
-				new ApiDeleteFailAction(failQuery));
-		});
-		expect(res).toBeDefined();
-	});
-
-	it('should respond to successfull LOCAL_QUERY_INIT action', () => {
-		let res;
-		let query = {
-			type: 'Article',
-			id: '1',
-			queryId: '11'
-		}
-		runner.queue(new LocalQueryInitAction(query));
-		effects.queryStore$.subscribe(result => {
-			res = result;
-			expect(result).toEqual(
-				new LocalQuerySuccessAction({
-					jsonApiData: { data: result.payload.jsonApiData.data },
-					query: query,
-				}));
-		});
-		expect(res).toBeDefined();
-	});
-
-});
+	/*it('should inject', inject([SampleService], (service: SampleService) => {
+		expect(service).toBeTruthy()
+	}))*/
+})
