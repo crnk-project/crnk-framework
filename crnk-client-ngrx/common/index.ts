3511be4ec8ad79d24c44023a879e568186576cee
remo <remo.meier@adnovum.ch>
2017-08-09 21:47:05 +0200

    ngrx setup

--- crnk-client-ngrx/common/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './arb.utils.value.accessor.base';
-export * from '../src/operations/arb.utils.json.api';
-export * from '../src/operations';
-export * from './error_router';
-export * from './loading';
--- crnk-client-ngrx/karma.conf.js
+++ crnk-client-ngrx/karma.conf.js
@@ -1,89 +1,37 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
-
-module.exports = function (config) {
+module.exports = function(config) {
 	config.set({
-		basePath: '',
+
 		frameworks: ["jasmine", "karma-typescript"],
 
-		plugins: [
-			require('karma-jasmine'),
-			//require('@angular/cli/plugins/karma'),
-			require('karma-chrome-launcher'),
-			require('karma-firefox-launcher'),
-			require('karma-jasmine-html-reporter'),
-			require('karma-coverage-istanbul-reporter'),
-			require('karma-junit-reporter'),
-			require('karma-typescript')
-			//require('karma-typescript-preprocessor')
-		],
-		client: {
-			// leave Jasmine Spec Runner output visible in browser
-			clearContext: false
-		},
 		files: [
-			{ pattern: "src/main/typescript/**/*.ts" },
-			{ pattern: "src/test/typescript/**/*.ts" }
-
-			// {pattern: './src/test/typescript/test.bundle.ts', watched: false}
+			{ pattern: "base.spec.ts" },
+			{ pattern: "src/main/typescript/**/*.+(ts|html)" }
 		],
-		preprocessors: {
-			"src/main/typescript/**/*.ts": ["karma-typescript"],
-			"test/test/typescript/**/*.ts": ["karma-typescript"]
 
-			// "**/*.ts": ["karma-typescript"],
-			// '**/*.ts': ['typescript']
+		preprocessors: {
+			"**/*.ts": ["karma-typescript"]
 		},
-		mime: {
-			'text/x-typescript': ['ts', 'tsx']
-		},
+
 		karmaTypescriptConfig: {
+
+
 			bundlerOptions: {
 				entrypoints: /\.spec\.ts$/,
 				transforms: [
-					require('karma-typescript-angular2-transform'),
-					require("karma-typescript-es6-transform")()
+					require("karma-typescript-es6-transform")({presets: ["es2015"]}),
+					require("karma-typescript-angular2-transform")
+
 				]
 			},
-
-
 			compilerOptions: {
-				lib: ['ES2015', 'DOM'],
-				module: "commonjs",
-				emitDecoratorMetadata: true,
-				experimentalDecorators: true,
-				sourceMap: true,
-				target: "ES5"
+				lib: ["ES2015", "DOM"]
 			}
 		},
 
-
-
-		coverageIstanbulReporter: {
-			reports: ['html', 'lcovonly'],
-			fixWebpackSourcePaths: true
-		},
-		// reporters: ['progress', 'junit'].concat(
-		//	config.angularCli && config.angularCli.codeCoverage ? ['coverage-istanbul'] : ['kjhtml']),
-
-		reporters: ['progress', 'karma-typescript'],
-
-		port: 9876,
-		colors: true,
-		logLevel: config.LOG_INFO,
-		autoWatch: true,
-		browsers: ['Firefox'],
 		singleRun: true,
-		junitReporter: {
-			outputDir: 'build/test/log/karma',
-			outputFile: 'unit.xml',
-			useBrowserName: false,
-			suite: 'asap-resource-browser-module',
-			nameFormatter: function classNameOmittingNameFormatter(browser, result) {
-				var descriptions = result.suite.slice(1);
-				descriptions.push(result.description);
-				return descriptions.join(' ');
-			}
-		}
+
+		reporters: ["dots", "karma-typescript"],
+
+		browsers: ["Chrome"]
 	});
 };
