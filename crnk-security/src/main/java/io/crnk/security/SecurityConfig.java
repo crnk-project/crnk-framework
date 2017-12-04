087f9606895f9e7ee110f7d7c3a090075cfaefdc
Victor Mosin <vicmosin@gmail.com>
2017-12-04 09:04:26 +0100

    Added chaining support Added multiple resource-permission definitions

--- crnk-security/src/main/java/io/crnk/security/SecurityConfig.java
+++ crnk-security/src/main/java/io/crnk/security/SecurityConfig.java
@@ -9,7 +9,7 @@ import java.util.List;
  */
 public class SecurityConfig {
 
-	private List<SecurityRule> rules = new ArrayList<>();
+	private List<SecurityRule> rules;
 
 	private SecurityConfig(List<SecurityRule> rules) {
 		this.rules = Collections.unmodifiableList(rules);
@@ -30,32 +30,56 @@ public class SecurityConfig {
 		private Builder() {
 		}
 
-		public void permitAll(ResourcePermission permission) {
-			rules.add(new SecurityRule(SecurityModule.ALL_ROLE, permission));
+		public Builder permitAll(ResourcePermission... permissions) {
+			for (ResourcePermission permission: permissions) {
+				rules.add(new SecurityRule(SecurityModule.ALL_ROLE, permission));
+			}
+
+			return this;
 		}
 
-		public <T> void permitAll(Class<T> resourceClass, ResourcePermission permission) {
-			permitRole(SecurityModule.ALL_ROLE, resourceClass, permission);
+		public <T> Builder permitAll(Class<T> resourceClass, ResourcePermission... permissions) {
+			for (ResourcePermission permission: permissions) {
+				permitRole(SecurityModule.ALL_ROLE, resourceClass, permission);
+			}
+
+			return this;
 		}
 
-		public void permitAll(String resourceType, ResourcePermission permission) {
-			rules.add(new SecurityRule(resourceType, SecurityModule.ALL_ROLE, permission));
+		public Builder permitAll(String resourceType, ResourcePermission... permissions) {
+			for (ResourcePermission permission: permissions) {
+				rules.add(new SecurityRule(resourceType, SecurityModule.ALL_ROLE, permission));
+			}
+
+			return this;
 		}
 
-		public void permitRole(String role, ResourcePermission permission) {
-			rules.add(new SecurityRule(role, permission));
+		public Builder permitRole(String role, ResourcePermission... permissions) {
+			for (ResourcePermission permission: permissions) {
+				rules.add(new SecurityRule(role, permission));
+			}
+
+			return this;
 		}
 
-		public <T> void permitRole(String role, Class<T> resourceClass, ResourcePermission permission) {
-			rules.add(new SecurityRule(resourceClass, role, permission));
+		public <T> Builder permitRole(String role, Class<T> resourceClass, ResourcePermission... permissions) {
+			for (ResourcePermission permission: permissions) {
+				rules.add(new SecurityRule(resourceClass, role, permission));
+			}
+
+			return this;
 		}
 
-		public void permitRole(String role, String resourceType, ResourcePermission permission) {
-			rules.add(new SecurityRule(resourceType, role, permission));
+		public Builder permitRole(String role, String resourceType, ResourcePermission... permissions) {
+			for (ResourcePermission permission: permissions) {
+				rules.add(new SecurityRule(resourceType, role, permission));
+			}
+
+			return this;
 		}
 
 		public SecurityConfig build() {
-			return new SecurityConfig(new ArrayList<SecurityRule>(rules));
+			return new SecurityConfig(rules);
 		}
 	}
 
