0e03a9b783bd1883432c77b2251db59331e10097
Victor Mosin <vicmosin@users.noreply.github.com>
2017-12-08 09:24:08 +0100

    use of validation factory in spring #176
    The issue is that validation module uses Validation.buildDefaultValidatorFactory() to get the validation factory. While spring implements it via LocalValidatorFactoryBean As a result, DI mechanism doesn't work inside validators i.e. none of the beans defined in spring could be used there.. I will try to create a PR during coming days. Implementation updated accordingly.

--- crnk-validation/src/main/java/io/crnk/validation/filter/ValidationRepositoryFilter.java
+++ crnk-validation/src/main/java/io/crnk/validation/filter/ValidationRepositoryFilter.java
@@ -10,16 +10,20 @@ import java.util.Set;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import javax.validation.Validation;
 import javax.validation.Validator;
 
 public class ValidationRepositoryFilter extends RepositoryFilterBase {
 
+	private final Validator validator;
+
+	public ValidationRepositoryFilter(final Validator validator) {
+		this.validator = validator;
+
+	}
 	@Override
 	public JsonApiResponse filterRequest(final RepositoryFilterContext context, final RepositoryRequestFilterChain chain) {
 		if (context.getRequest().getMethod() == HttpMethod.POST ||
 				context.getRequest().getMethod() == HttpMethod.PATCH || context.getRequest().getMethod() == HttpMethod.PUT) {
-			Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 			Set<ConstraintViolation<Object>> violations = validator.validate(context.getRequest().getEntity());
 			if (!violations.isEmpty()) {
 				throw new ConstraintViolationException(violations);
