b76d5c44c37696984cbe7a06c827e0e184dc214e
Remo Meier <remo.meier@adnvoum.ch>
2018-02-05 19:38:27 +0100

    support jsonapi-style home documents in crnk-home #213

--- crnk-core/src/main/java/io/crnk/core/engine/http/HttpHeaders.java
+++ crnk-core/src/main/java/io/crnk/core/engine/http/HttpHeaders.java
@@ -13,6 +13,8 @@ public class HttpHeaders {
 
 	public static final String JSONAPI_CONTENT_TYPE = "application/vnd.api+json";
 
+	public static final String JSON_CONTENT_TYPE = "application/json";
+
 	public static final String DEFAULT_CHARSET = "utf-8";
 
 	public static final String JSONAPI_CONTENT_TYPE_AND_CHARSET = JSONAPI_CONTENT_TYPE + "; charset=" +
--- crnk-home/src/main/java/io/crnk/home/HomeModule.java
+++ crnk-home/src/main/java/io/crnk/home/HomeModule.java
@@ -1,13 +1,18 @@
 package io.crnk.home;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.sun.javafx.scene.shape.PathUtils;
 import io.crnk.core.engine.filter.FilterBehavior;
+import io.crnk.core.engine.http.HttpHeaders;
 import io.crnk.core.engine.http.HttpMethod;
 import io.crnk.core.engine.http.HttpRequestContext;
 import io.crnk.core.engine.http.HttpRequestProcessor;
 import io.crnk.core.engine.information.repository.RepositoryInformation;
 import io.crnk.core.engine.information.resource.ResourceInformation;
+import io.crnk.core.engine.internal.dispatcher.path.JsonPath;
+import io.crnk.core.engine.internal.dispatcher.path.PathBuilder;
 import io.crnk.core.engine.internal.utils.UrlUtils;
 import io.crnk.core.engine.registry.RegistryEntry;
 import io.crnk.core.engine.registry.ResourceRegistry;
@@ -20,6 +25,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Displays a list of available resources in the root directory.
@@ -32,26 +38,41 @@ public class HomeModule implements Module, ModuleExtensionAware<HomeModuleExtens
 
 	private List<HomeModuleExtension> extensions;
 
+	private HomeFormat defaultFormat;
+
+	private ModuleContext moduleContext;
+
 	// protected for CDI
 	protected HomeModule() {
 	}
 
 	public static HomeModule create() {
-		return new HomeModule();
+		return create(HomeFormat.JSON_API);
 	}
 
-	public static boolean isHomeRequest(HttpRequestContext requestContext) {
-		boolean isRoot = UrlUtils.removeTrailingSlash(requestContext.getPath()).isEmpty();
-		if (!isRoot) {
+	public static HomeModule create(HomeFormat format) {
+		HomeModule module = new HomeModule();
+		module.defaultFormat = format;
+		return module;
+	}
+
+	private boolean isHomeRequest(HttpRequestContext requestContext) {
+		String path = requestContext.getPath();
+		if (!path.endsWith("/") || !requestContext.getMethod().equalsIgnoreCase(HttpMethod.GET.toString())) {
 			return false;
 		}
 
 		boolean acceptsHome = requestContext.accepts(JSON_HOME_CONTENT_TYPE);
+		boolean acceptsJsonApi = requestContext.accepts(HttpHeaders.JSONAPI_CONTENT_TYPE);
+		boolean acceptsJson = requestContext.accepts(HttpHeaders.JSON_CONTENT_TYPE);
 		boolean acceptsAny = requestContext.acceptsAny();
-		if (!(acceptsHome || acceptsAny)) {
+		if (!(acceptsHome || acceptsAny || acceptsJson || acceptsJsonApi)) {
 			return false;
 		}
-		return requestContext.getMethod().equalsIgnoreCase(HttpMethod.GET.toString());
+
+		ResourceRegistry resourceRegistry = moduleContext.getResourceRegistry();
+		JsonPath jsonPath = new PathBuilder(resourceRegistry).build(path);
+		return jsonPath == null; // no repository with that path
 	}
 
 	@Override
@@ -61,14 +82,11 @@ public class HomeModule implements Module, ModuleExtensionAware<HomeModuleExtens
 
 	@Override
 	public void setupModule(final ModuleContext context) {
+		this.moduleContext = context;
 		context.addHttpRequestProcessor(new HttpRequestProcessor() {
 			@Override
 			public void process(HttpRequestContext requestContext) throws IOException {
 				if (isHomeRequest(requestContext)) {
-					ObjectMapper objectMapper = context.getObjectMapper();
-					ObjectNode node = objectMapper.createObjectNode();
-					ObjectNode resourcesNode = node.putObject("resources");
-
 					Set<String> pathSet = new HashSet<>();
 
 					ResourceRegistry resourceRegistry = context.getResourceRegistry();
@@ -89,27 +107,80 @@ public class HomeModule implements Module, ModuleExtensionAware<HomeModuleExtens
 						}
 					}
 
+					String requestPath = requestContext.getPath();
+					pathSet = pathSet.stream().map(it -> getChildName(requestPath, it))
+							.filter(it -> it != null).collect(Collectors.toSet());
+
 					List<String> pathList = new ArrayList<>(pathSet);
 					Collections.sort(pathList);
-					for (String path : pathList) {
-						String tag = "tag:" + path;
-						String href = path;
-						ObjectNode resourceNode = resourcesNode.putObject(tag);
-						resourceNode.put("href", href);
-					}
 
-					String json = objectMapper.writeValueAsString(node);
-					boolean acceptsHome = requestContext.accepts(JSON_HOME_CONTENT_TYPE);
-					if (acceptsHome) {
-						requestContext.setContentType(JSON_HOME_CONTENT_TYPE);
+					if (defaultFormat == HomeFormat.JSON_HOME || requestContext.accepts(JSON_HOME_CONTENT_TYPE)) {
+						boolean acceptsHome = requestContext.accepts(JSON_HOME_CONTENT_TYPE);
+						if (acceptsHome) {
+							requestContext.setContentType(JSON_HOME_CONTENT_TYPE);
+						}
+						else {
+							requestContext.setContentType(JSON_CONTENT_TYPE);
+						}
+						writeJsonHome(requestContext, pathList);
 					}
 					else {
-						requestContext.setContentType(JSON_CONTENT_TYPE);
+						boolean jsonapi = requestContext.accepts(HttpHeaders.JSONAPI_CONTENT_TYPE);
+						if (jsonapi) {
+							requestContext.setContentType(HttpHeaders.JSONAPI_CONTENT_TYPE);
+						}
+						else {
+							requestContext.setContentType(JSON_CONTENT_TYPE);
+						}
+						writeJsonApi(requestContext, pathList);
 					}
-					requestContext.setResponse(200, json);
+
 				}
 			}
+
+			private String getChildName(String requestPath, String it) {
+				if (it.startsWith(requestPath)) {
+					String subPath = UrlUtils.removeTrailingSlash(it.substring(requestPath.length()));
+					int sep = subPath.indexOf('/');
+					return sep == -1 ? subPath : subPath.substring(0, sep) + "/";
+				}
+				return null;
+			}
 		});
+	}
+
+	private void writeJsonApi(HttpRequestContext requestContext, List<String> pathList) throws IOException {
+		ObjectMapper objectMapper = moduleContext.getObjectMapper();
+
+		String baseUrl = UrlUtils.removeTrailingSlash(moduleContext.getResourceRegistry().getServiceUrlProvider().getUrl())
+				+ requestContext.getPath();
+
+		ObjectNode node = objectMapper.createObjectNode();
+		ObjectNode links = node.putObject("links");
+		for (String path : pathList) {
+			String href = baseUrl + path;
+			String id = UrlUtils.removeTrailingSlash(path);
+			links.put(id, href);
+		}
+
+		String json = objectMapper.writeValueAsString(node);
+		requestContext.setResponse(200, json);
+	}
+
+	private void writeJsonHome(HttpRequestContext requestContext, List<String> pathList) throws IOException {
+		ObjectMapper objectMapper = moduleContext.getObjectMapper();
+
+		ObjectNode node = objectMapper.createObjectNode();
+		ObjectNode resourcesNode = node.putObject("resources");
+		for (String path : pathList) {
+			String tag = "tag:" + UrlUtils.removeTrailingSlash(path);
+			String href = path;
+			ObjectNode resourceNode = resourcesNode.putObject(tag);
+			resourceNode.put("href", href);
+		}
+		String json = objectMapper.writeValueAsString(node);
+		System.out.println(json);
+		requestContext.setResponse(200, json);
 	}
 
 
