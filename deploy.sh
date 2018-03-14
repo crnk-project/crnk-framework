8fd23e2bca544fb28ad60e8aedb4cd5d81025bad
Remo Meier <remo.meier@adnovum.ch>
2018-03-14 13:35:39 +0100

    parallel build enabled

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew publish --max-workers=1
+  ./gradlew publish --max-workers=1 --no-parallel
 else
   echo "Skip deploying";
 fi
