f9da81f2f1af68091862d25738aa248e69f4f075
Jan Willem Harmelink <janwillem.harmelink@devolksbank.nl>
2018-05-15 16:22:37 +0200

    Used ResourceRegistry to derive self link and added unit test

--- crnk-core/src/main/java/io/crnk/core/engine/internal/repository/RepositoryAdapterUtils.java
+++ crnk-core/src/main/java/io/crnk/core/engine/internal/repository/RepositoryAdapterUtils.java
@@ -2,9 +2,7 @@ package io.crnk.core.engine.internal.repository;
 
 import io.crnk.core.engine.dispatcher.RepositoryRequestSpec;
 import io.crnk.core.engine.information.resource.ResourceInformation;
-import io.crnk.core.engine.internal.utils.UrlUtils;
-import io.crnk.core.engine.query.QueryAdapter;
-import io.crnk.core.engine.url.ServiceUrlProvider;
+import io.crnk.core.engine.registry.ResourceRegistry;
 import io.crnk.core.module.ModuleRegistry;
 import io.crnk.core.queryspec.internal.QuerySpecAdapter;
 import io.crnk.core.queryspec.pagingspec.PagingBehavior;
@@ -17,48 +15,42 @@ import io.crnk.core.resource.list.ResourceList;
 
 public class RepositoryAdapterUtils {
 
-	public static LinksInformation enrichLinksInformation(ModuleRegistry moduleRegistry, LinksInformation linksInformation,
-			Object resources,
-			RepositoryRequestSpec requestSpec) {
-		QueryAdapter queryAdapter = requestSpec.getQueryAdapter();
-		LinksInformation enrichedLinksInformation = linksInformation;
-		if (queryAdapter instanceof QuerySpecAdapter && resources instanceof ResourceList) {
-			ResourceInformation responseResourceInformation = requestSpec.getResponseResourceInformation();
-			PagingBehavior pagingBehavior = responseResourceInformation.getPagingBehavior();
-			if (pagingBehavior != null && pagingBehavior.isRequired(queryAdapter.getPagingSpec())) {
-				enrichedLinksInformation =
-						enrichPageLinksInformation(moduleRegistry, enrichedLinksInformation, (ResourceList<?>) resources,
-								queryAdapter, requestSpec);
-			}
-		}
-		return enrichedLinksInformation;
-	}
-
-	private static LinksInformation enrichPageLinksInformation(ModuleRegistry moduleRegistry, LinksInformation linksInformation,
-			ResourceList<?> resources,
-			QueryAdapter queryAdapter, RepositoryRequestSpec requestSpec) {
-		if (linksInformation == null) {
-			// use default implementation if no link information
-			// provided by resource
-			linksInformation = new DefaultPagedLinksInformation();
-		} else if (linksInformation instanceof SelfLinksInformation) {
-			SelfLinksInformation selfLinksInformation = (SelfLinksInformation) linksInformation;
-			selfLinksInformation.setSelf(getSelfUrl(moduleRegistry, requestSpec));
-			return selfLinksInformation;
-		}
-		if (linksInformation instanceof PagedLinksInformation) {
-			PagingSpecUrlBuilder urlBuilder = new PagingSpecUrlBuilder(moduleRegistry, requestSpec);
-			requestSpec.getResponseResourceInformation().getPagingBehavior()
-					.build((PagedLinksInformation) linksInformation, resources, queryAdapter, urlBuilder);
+	public static LinksInformation enrichLinksInformation(ModuleRegistry moduleRegistry,
+														  LinksInformation linksInformation, Object resource,
+														  RepositoryRequestSpec requestSpec) {
+		if (requestSpec.getQueryAdapter() instanceof QuerySpecAdapter && resource instanceof ResourceList) {
+			ResourceList<?> resources = (ResourceList<?>) resource;
+			linksInformation = enrichPageLinksInformation(moduleRegistry, linksInformation, resources, requestSpec);
+			linksInformation = enrichSelfLinksInformation(linksInformation, requestSpec);
 		}
 		return linksInformation;
 	}
 
-	private static String getSelfUrl(ModuleRegistry moduleRegistry, RepositoryRequestSpec requestSpec) {
-		ResourceInformation resourceInformation = requestSpec.getResponseResourceInformation();
-		ServiceUrlProvider serviceUrlProvider = moduleRegistry.getHttpRequestContextProvider().getServiceUrlProvider();
-		String url = UrlUtils.removeTrailingSlash(serviceUrlProvider.getUrl());
-		String resourcePath = resourceInformation.getResourcePath() == null ? resourceInformation.getResourceType() : resourceInformation.getResourcePath();
-		return url != null ? url + "/" + resourcePath : null;
+	private static LinksInformation enrichSelfLinksInformation(LinksInformation linksInformation,
+															   RepositoryRequestSpec requestSpec) {
+		ResourceInformation resourceInformation = requestSpec.getQueryAdapter().getResourceInformation();
+		ResourceRegistry resourceRegistry = requestSpec.getQueryAdapter().getResourceRegistry();
+		if (resourceRegistry != null && resourceInformation != null && linksInformation instanceof SelfLinksInformation) {
+			((SelfLinksInformation) linksInformation).setSelf(resourceRegistry.getResourceUrl(resourceInformation));
+		}
+		return linksInformation;
+	}
+
+	private static LinksInformation enrichPageLinksInformation(ModuleRegistry moduleRegistry,
+															   LinksInformation linksInformation,
+															   ResourceList<?> resources,
+															   RepositoryRequestSpec requestSpec) {
+		PagingBehavior pagingBehavior = requestSpec.getResponseResourceInformation().getPagingBehavior();
+		if (pagingBehavior != null && pagingBehavior.isRequired(requestSpec.getQueryAdapter().getPagingSpec())) {
+			if (linksInformation == null) {
+				// use default implementation if no link information provided by resource
+				linksInformation = new DefaultPagedLinksInformation();
+			}
+			if (linksInformation instanceof PagedLinksInformation) {
+				PagingSpecUrlBuilder urlBuilder = new PagingSpecUrlBuilder(moduleRegistry, requestSpec);
+				pagingBehavior.build((PagedLinksInformation) linksInformation, resources, requestSpec.getQueryAdapter(), urlBuilder);
+			}
+		}
+		return linksInformation;
 	}
 }
