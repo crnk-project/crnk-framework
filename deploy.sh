736e04d63783236d5ac509f3a3d8afb0c8673ae7
Remo <remo.meier@adnovum.ch>
2018-05-23 11:28:34 +0200

    publish fixes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "stable" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew publish promote
+  ./gradlew publish promote --max-workers=1 --no-parallel
 else
   echo "Skip deploying";
 fi
