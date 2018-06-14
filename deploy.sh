12094d41d96ab075588c15cde97615ee4a4009a3
Remo <remo.meier@adnovum.ch>
2018-06-14 10:58:49 +0200

    publish builds from master branch to bintray mavenLatest repository #323
    published to https://bintray.com/crnk-project/mavenLatest. Also updates latest release information.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "stable" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
   ./gradlew publish promote --max-workers=1 --no-parallel
+elif [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  ./gradlew publish --max-workers=1 --no-parallel
 else
   echo "Skip deploying";
 fi
