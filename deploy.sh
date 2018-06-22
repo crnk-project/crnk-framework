39e6d9404d625819353d4cfd37541ff37414b3dd
Remo <remo.meier@adnovum.ch>
2018-06-22 14:32:44 +0200

    updated release promotion mechanism
    travis does not give access to branch name.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "stable" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew publish promote --max-workers=1 --no-parallel
+  ./gradlew publish promote --max-workers=1 --no-parallel -Pstable=true
 elif [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
   ./gradlew publish --max-workers=1 --no-parallel
 else
