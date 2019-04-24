a695aae6e293101934f5708823f48a0c6c34ea5e
Remo <remo.meier@adnovum.ch>
2019-04-24 10:09:53 +0200

    generator class path fix #237
    MetaLookup not allowed to be in contract between generator and runtime, causes to many ClassLoader dependencies.

--- crnk-gen/crnk-gen-runtime/src/main/java/io/crnk/gen/runtime/RuntimeContext.java
+++ crnk-gen/crnk-gen-runtime/src/main/java/io/crnk/gen/runtime/RuntimeContext.java
@@ -1,34 +1,37 @@
 package io.crnk.gen.runtime;
 
+import java.io.IOException;
+
 import io.crnk.gen.base.GeneratorConfig;
 import io.crnk.gen.base.GeneratorModule;
-import io.crnk.meta.MetaLookup;
-
-import java.io.IOException;
 
 public class RuntimeContext {
 
-    private final GeneratorModule module;
+	private final GeneratorModule module;
 
-    private final GeneratorConfig config;
+	private final GeneratorConfig config;
 
-    private final ClassLoader classloader;
+	private final ClassLoader classloader;
 
-    public RuntimeContext(GeneratorModule module, ClassLoader classloader, GeneratorConfig config) {
-        this.config = config;
-        this.module = module;
-        this.classloader = classloader;
-    }
+	public RuntimeContext(GeneratorModule module, ClassLoader classloader, GeneratorConfig config) {
+		this.config = config;
+		this.module = module;
+		this.classloader = classloader;
+	}
 
-    public GeneratorConfig getConfig() {
-        return config;
-    }
+	public GeneratorConfig getConfig() {
+		return config;
+	}
 
-    public void generate(MetaLookup lookup) throws IOException {
-        module.generate(lookup);
-    }
+	/**
+	 * @param lookup provided as java.lang.Object to minimize generator class loading contract
+	 * @throws IOException
+	 */
+	public void generate(Object lookup) throws IOException {
+		module.generate(lookup);
+	}
 
-    public ClassLoader getClassLoader() {
-        return classloader;
-    }
+	public ClassLoader getClassLoader() {
+		return classloader;
+	}
 }
