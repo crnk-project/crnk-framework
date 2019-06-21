04c7fbda03ee7d4491497a0adac5d8c33f29be6f
Jochen Reinhardt <46705439+JochenReinhardt@users.noreply.github.com>
2019-06-21 20:49:05 +0200

    #525 Option in Operations module to resume processing in case of an error to achieve non-transactional behavior

---
+++ crnk-operations/src/test/java/io/crnk/operations/NonTransactionalOperationsExceptionTest.java
@@ -0,0 +1,54 @@
+package io.crnk.operations;
+
+import io.crnk.core.engine.http.HttpMethod;
+import io.crnk.core.engine.http.HttpStatus;
+import io.crnk.core.queryspec.QuerySpec;
+import io.crnk.core.repository.ResourceRepository;
+import io.crnk.operations.client.OperationsCall;
+import io.crnk.operations.client.OperationsClient;
+import io.crnk.operations.model.PersonEntity;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+public class NonTransactionalOperationsExceptionTest extends AbstractOperationsTest {
+
+	private OperationsClient operationsClient;
+
+	public NonTransactionalOperationsExceptionTest() {
+		transactional = false;
+	}
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		operationsClient = new OperationsClient(client);
+	}
+
+	@Test
+	public void testResumeOnError() {
+		operationsModule.setResumeOnError(true);
+
+		PersonEntity person1 = newPerson("1");
+		PersonEntity person2 = newPerson("2");
+
+		person1.setName(null); // trigger validation error
+
+		OperationsCall insertCall = operationsClient.createCall();
+		insertCall.add(HttpMethod.POST, person1);
+		insertCall.add(HttpMethod.POST, person2);
+		insertCall.execute();
+
+		Assert.assertEquals(HttpStatus.UNPROCESSABLE_ENTITY_422, insertCall.getResponse(0).getStatus());
+		Assert.assertEquals(HttpStatus.CREATED_201, insertCall.getResponse(1).getStatus());
+
+		QuerySpec querySpec = new QuerySpec(PersonEntity.class);
+		ResourceRepository<PersonEntity, UUID> personRepo = client.getRepositoryForType(PersonEntity.class);
+		List<PersonEntity> list = personRepo.findAll(querySpec);
+		Assert.assertEquals(1, list.size());
+	}
+}
