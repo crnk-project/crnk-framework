e458e46388fe4c60b42444164635f738935abe09
Remo <remo.meier@adnovum.ch>
2019-09-27 13:19:11 +0200

    use @JsonApiExposed(false) for client repository stubs
    by default client stubs should not be exposed on the
    server-side (e.g. when setup and discovered by dependency injection).
    Most likely the server wants to use the stubs to call other services,
    not expose the same services again.
    
    For this purpose the @JsonApiExposed logic needed to improve to detect
    wrappers and dynamic stubs need to implement the Wrapper contract.

--- crnk-client/src/main/java/io/crnk/client/internal/ResourceRepositoryStubImpl.java
+++ crnk-client/src/main/java/io/crnk/client/internal/ResourceRepositoryStubImpl.java
@@ -1,5 +1,8 @@
 package io.crnk.client.internal;
 
+import java.util.Collection;
+import java.util.concurrent.Callable;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.crnk.client.ClientFormat;
 import io.crnk.client.CrnkClient;
@@ -17,126 +20,126 @@ import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.queryspec.internal.QuerySpecAdapter;
 import io.crnk.core.repository.ResourceRepository;
 import io.crnk.core.repository.response.JsonApiResponse;
+import io.crnk.core.resource.annotations.JsonApiExposed;
 import io.crnk.core.resource.list.DefaultResourceList;
 
-import java.io.Serializable;
-import java.util.Collection;
-import java.util.concurrent.Callable;
+@JsonApiExposed(false)
+public class ResourceRepositoryStubImpl<T, I> extends ClientStubBase
+		implements ResourceRepository<T, I> {
 
-public class ResourceRepositoryStubImpl<T, I > extends ClientStubBase
-        implements ResourceRepository<T, I> {
-
-    protected ResourceInformation resourceInformation;
+	protected ResourceInformation resourceInformation;
 
 
-    public ResourceRepositoryStubImpl(CrnkClient client, Class<T> resourceClass, ResourceInformation resourceInformation,
-                                      JsonApiUrlBuilder urlBuilder) {
-        super(client, urlBuilder, resourceClass);
-        this.resourceInformation = resourceInformation;
-    }
+	public ResourceRepositoryStubImpl(CrnkClient client, Class<T> resourceClass, ResourceInformation resourceInformation,
+			JsonApiUrlBuilder urlBuilder) {
+		super(client, urlBuilder, resourceClass);
+		this.resourceInformation = resourceInformation;
+	}
 
-    private Object executeUpdate(String requestUrl, T resource, boolean create) {
-        JsonApiResponse response = new JsonApiResponse();
-        response.setEntity(resource);
+	private Object executeUpdate(String requestUrl, T resource, boolean create) {
+		JsonApiResponse response = new JsonApiResponse();
+		response.setEntity(resource);
 
-        ClientDocumentMapper documentMapper = client.getDocumentMapper();
-        DocumentMappingConfig mappingConfig = new DocumentMappingConfig();
+		ClientDocumentMapper documentMapper = client.getDocumentMapper();
+		DocumentMappingConfig mappingConfig = new DocumentMappingConfig();
 
-        // do not write empty values like 0 and false => not necessary
-        mappingConfig.getResourceMapping().setIgnoreDefaults(create);
+		// do not write empty values like 0 and false => not necessary
+		mappingConfig.getResourceMapping().setIgnoreDefaults(create);
 
-        ClientFormat format = client.getFormat();
-        QueryAdapter queryAdapter = new QuerySpecAdapter(null, client.getRegistry(), client.getQueryContext());
-        final Document requestDocument = format.toTransportDocument(documentMapper.toDocument(response, queryAdapter, mappingConfig).get());
+		ClientFormat format = client.getFormat();
+		QueryAdapter queryAdapter = new QuerySpecAdapter(null, client.getRegistry(), client.getQueryContext());
+		final Document requestDocument = format.toTransportDocument(documentMapper.toDocument(response, queryAdapter, mappingConfig).get());
 
-        final ObjectMapper objectMapper = client.getObjectMapper();
-        String requestBodyValue = ExceptionUtil.wrapCatchedExceptions(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                return objectMapper.writeValueAsString(requestDocument);
-            }
-        });
+		final ObjectMapper objectMapper = client.getObjectMapper();
+		String requestBodyValue = ExceptionUtil.wrapCatchedExceptions(new Callable<String>() {
+			@Override
+			public String call() throws Exception {
+				return objectMapper.writeValueAsString(requestDocument);
+			}
+		});
 
-        HttpMethod method = create ? HttpMethod.POST : HttpMethod.PATCH;
+		HttpMethod method = create ? HttpMethod.POST : HttpMethod.PATCH;
 
-        return execute(requestUrl, ResponseType.RESOURCE, method, requestBodyValue);
-    }
+		return execute(requestUrl, ResponseType.RESOURCE, method, requestBodyValue);
+	}
 
 
-    @Override
-    public <S extends T> S save(S entity) {
-        return modify(entity, false);
-    }
+	@Override
+	public <S extends T> S save(S entity) {
+		return modify(entity, false);
+	}
 
-    @SuppressWarnings("unchecked")
-    private <S extends T> S modify(S entity, boolean create) {
-        Object id = getId(entity);
+	@SuppressWarnings("unchecked")
+	private <S extends T> S modify(S entity, boolean create) {
+		Object id = getId(entity);
 
-        if (create && !resourceInformation.isNested()) {
-            id = null;
-        } else if (!create && id == null) {
-            throw new BadRequestException("id must not be null for " + entity);
-        }
+		if (create && !resourceInformation.isNested()) {
+			id = null;
+		}
+		else if (!create && id == null) {
+			throw new BadRequestException("id must not be null for " + entity);
+		}
 
-        String url = urlBuilder.buildUrl(resourceInformation, id, (QuerySpec) null);
-        if (create && resourceInformation.isNested() && !resourceInformation.isSingularNesting() && id != null) {
-            // for multi-valued nested resource drop the nested id part
-            url = url.substring(0, url.lastIndexOf('/'));
-        }
-        return (S) executeUpdate(url, entity, create);
-    }
+		String url = urlBuilder.buildUrl(resourceInformation, id, (QuerySpec) null);
+		if (create && resourceInformation.isNested() && !resourceInformation.isSingularNesting() && id != null) {
+			// for multi-valued nested resource drop the nested id part
+			url = url.substring(0, url.lastIndexOf('/'));
+		}
+		return (S) executeUpdate(url, entity, create);
+	}
 
-    @Override
-    public <S extends T> S create(S entity) {
-        return modify(entity, true);
-    }
+	@Override
+	public <S extends T> S create(S entity) {
+		return modify(entity, true);
+	}
 
-    private <S extends T> Object getId(S entity) {
-        if (entity instanceof Resource) {
-            return ((Resource) entity).getId();
-        } else {
-            ResourceField idField = resourceInformation.getIdField();
-            return idField.getAccessor().getValue(entity);
-        }
-    }
+	private <S extends T> Object getId(S entity) {
+		if (entity instanceof Resource) {
+			return ((Resource) entity).getId();
+		}
+		else {
+			ResourceField idField = resourceInformation.getIdField();
+			return idField.getAccessor().getValue(entity);
+		}
+	}
 
-    @Override
-    public void delete(I id) {
-        String url = urlBuilder.buildUrl(resourceInformation, id, (QuerySpec) null);
-        executeDelete(url);
-    }
+	@Override
+	public void delete(I id) {
+		String url = urlBuilder.buildUrl(resourceInformation, id, (QuerySpec) null);
+		executeDelete(url);
+	}
 
-    @Override
-    public Class<T> getResourceClass() {
-        return (Class<T>) resourceClass;
-    }
+	@Override
+	public Class<T> getResourceClass() {
+		return (Class<T>) resourceClass;
+	}
 
-    @Override
-    public T findOne(I id, QuerySpec querySpec) {
-        String url = urlBuilder.buildUrl(resourceInformation, id, querySpec);
-        return findOne(url);
-    }
+	@Override
+	public T findOne(I id, QuerySpec querySpec) {
+		String url = urlBuilder.buildUrl(resourceInformation, id, querySpec);
+		return findOne(url);
+	}
 
-    @Override
-    public DefaultResourceList<T> findAll(QuerySpec querySpec) {
-        String url = urlBuilder.buildUrl(resourceInformation, null, querySpec);
-        return findAll(url);
-    }
+	@Override
+	public DefaultResourceList<T> findAll(QuerySpec querySpec) {
+		String url = urlBuilder.buildUrl(resourceInformation, null, querySpec);
+		return findAll(url);
+	}
 
-    @Override
-    public DefaultResourceList<T> findAll(Collection<I> ids, QuerySpec queryPaquerySpecrams) {
-        String url = urlBuilder.buildUrl(resourceInformation, ids, queryPaquerySpecrams);
-        return findAll(url);
-    }
+	@Override
+	public DefaultResourceList<T> findAll(Collection<I> ids, QuerySpec queryPaquerySpecrams) {
+		String url = urlBuilder.buildUrl(resourceInformation, ids, queryPaquerySpecrams);
+		return findAll(url);
+	}
 
-    @SuppressWarnings("unchecked")
-    public DefaultResourceList<T> findAll(String url) {
-        return (DefaultResourceList<T>) executeGet(url, ResponseType.RESOURCES);
-    }
+	@SuppressWarnings("unchecked")
+	public DefaultResourceList<T> findAll(String url) {
+		return (DefaultResourceList<T>) executeGet(url, ResponseType.RESOURCES);
+	}
 
-    @SuppressWarnings("unchecked")
-    protected T findOne(String url) {
-        return (T) executeGet(url, ResponseType.RESOURCE);
-    }
+	@SuppressWarnings("unchecked")
+	protected T findOne(String url) {
+		return (T) executeGet(url, ResponseType.RESOURCE);
+	}
 
 }
