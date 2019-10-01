55de2f12bd509932d8f3058bac47ebf7a614419e
MacLeod Broad <macleod.broad@workiva.com>
2019-10-01 14:33:58 -0400

    Fix formatting of SimpleTask

--- crnk-test/src/main/java/io/crnk/test/mock/models/SimpleTask.java
+++ crnk-test/src/main/java/io/crnk/test/mock/models/SimpleTask.java
@@ -12,72 +12,72 @@ import io.crnk.core.resource.meta.MetaInformation;
 @JsonApiResource(type = "simpleTasks", pagingSpec = OffsetLimitPagingSpec.class)
 public class SimpleTask {
 
-	@JsonApiId
-	private Long id;
+  @JsonApiId
+  private Long id;
 
-	private String name;
+  private String name;
 
-	@JsonApiMetaInformation
-	private SimpleTaskMeta metaInformation;
+  @JsonApiMetaInformation
+  private SimpleTaskMeta metaInformation;
 
-	@JsonApiLinksInformation
-	private SimpleTaskLinks linksInformation;
+  @JsonApiLinksInformation
+  private SimpleTaskLinks linksInformation;
 
-	public static class SimpleTaskLinks implements LinksInformation, SelfLinksInformation {
+  public static class SimpleTaskLinks implements LinksInformation, SelfLinksInformation {
 
-		public String value = "test";
+    public String value = "test";
 
-		public String self;
+    public String self;
 
-		@Override
-		public String getSelf() {
-			return self;
-		}
+    @Override
+    public String getSelf() {
+      return self;
+    }
 
-		@Override
-		public void setSelf(String self) {
-			this.self = self;
-		}
-	}
+    @Override
+    public void setSelf(String self) {
+      this.self = self;
+    }
+  }
 
-	public static class SimpleTaskMeta implements MetaInformation {
+  public static class SimpleTaskMeta implements MetaInformation {
 
-		public String value = "test";
+    public String value = "test";
 
-	}
+  }
 
-	public Long getId() {
-		return id;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public SimpleTask setId(Long id) {
-		this.id = id;
-		return this;
-	}
+  public SimpleTask setId(Long id) {
+    this.id = id;
+    return this;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setName(@SuppressWarnings("SameParameterValue") String name) {
-		this.name = name;
-	}
+  public void setName(@SuppressWarnings("SameParameterValue") String name) {
+    this.name = name;
+  }
 
-	public SimpleTaskMeta getMetaInformation() {
-		return metaInformation;
-	}
+  public SimpleTaskMeta getMetaInformation() {
+    return metaInformation;
+  }
 
-	public SimpleTask setMetaInformation(SimpleTaskMeta metaInformation) {
-		this.metaInformation = metaInformation;
-		return this;
-	}
+  public SimpleTask setMetaInformation(SimpleTaskMeta metaInformation) {
+    this.metaInformation = metaInformation;
+    return this;
+  }
 
-	public SimpleTaskLinks getLinksInformation() {
-		return linksInformation;
-	}
+  public SimpleTaskLinks getLinksInformation() {
+    return linksInformation;
+  }
 
-	public SimpleTask setLinksInformation(SimpleTaskLinks linksInformation) {
-		this.linksInformation = linksInformation;
-		return this;
-	}
+  public SimpleTask setLinksInformation(SimpleTaskLinks linksInformation) {
+    this.linksInformation = linksInformation;
+    return this;
+  }
 }
--- crnk-test/src/main/java/io/crnk/test/mock/repository/SimpleTaskRepository.java
+++ crnk-test/src/main/java/io/crnk/test/mock/repository/SimpleTaskRepository.java
@@ -15,68 +15,68 @@ import java.util.concurrent.ConcurrentHashMap;
 @JsonApiExposed
 public class SimpleTaskRepository implements ResourceRepository<SimpleTask, Long> {
 
-	private static final ConcurrentHashMap<Long, SimpleTask> map = new ConcurrentHashMap<>();
+  private static final ConcurrentHashMap<Long, SimpleTask> map = new ConcurrentHashMap<>();
 
-	public static void clear() {
-		map.clear();
-	}
+  public static void clear() {
+    map.clear();
+  }
 
-	public <S extends SimpleTask> S save(S entity) {
+  public <S extends SimpleTask> S save(S entity) {
 
-		if (entity.getId() == null) {
-			entity.setId((long) (map.size() + 1));
-		}
-		map.put(entity.getId(), entity);
+    if (entity.getId() == null) {
+      entity.setId((long) (map.size() + 1));
+    }
+    map.put(entity.getId(), entity);
 
-		return entity;
-	}
+    return entity;
+  }
 
-	@Override
-	public <S extends SimpleTask> S create(S resource) {
-		return save(resource);
-	}
+  @Override
+  public <S extends SimpleTask> S create(S resource) {
+    return save(resource);
+  }
 
-	@Override
-	public Class<SimpleTask> getResourceClass() {
-		return SimpleTask.class;
-	}
+  @Override
+  public Class<SimpleTask> getResourceClass() {
+    return SimpleTask.class;
+  }
 
-	public SimpleTask findOne(Long aLong, QuerySpec querySpec) {
-		SimpleTask task = map.get(aLong);
-		if (task == null) {
-			throw new ResourceNotFoundException("failed to find resource with id " + aLong);
-		}
-		return task;
-	}
+  public SimpleTask findOne(Long aLong, QuerySpec querySpec) {
+    SimpleTask task = map.get(aLong);
+    if (task == null) {
+      throw new ResourceNotFoundException("failed to find resource with id " + aLong);
+    }
+    return task;
+  }
 
-	@Override
-	public ResourceList<SimpleTask> findAll(QuerySpec querySpec) {
-		return querySpec.apply(map.values());
-	}
+  @Override
+  public ResourceList<SimpleTask> findAll(QuerySpec querySpec) {
+    return querySpec.apply(map.values());
+  }
 
-	@Override
-	public ResourceList<SimpleTask> findAll(Collection<Long> ids, QuerySpec queryParams) {
-		List<SimpleTask> querySpec = new LinkedList<>();
-		for (SimpleTask value : map.values()) {
-			if (contains(value, ids)) {
-				querySpec.add(value);
-			}
-		}
-		return queryParams.apply(querySpec);
-	}
+  @Override
+  public ResourceList<SimpleTask> findAll(Collection<Long> ids, QuerySpec queryParams) {
+    List<SimpleTask> querySpec = new LinkedList<>();
+    for (SimpleTask value : map.values()) {
+      if (contains(value, ids)) {
+        querySpec.add(value);
+      }
+    }
+    return queryParams.apply(querySpec);
+  }
 
-	private boolean contains(SimpleTask value, Collection<Long> ids) {
-		for (Long id : ids) {
-			if (value.getId().equals(id)) {
-				return true;
-			}
-		}
+  private boolean contains(SimpleTask value, Collection<Long> ids) {
+    for (Long id : ids) {
+      if (value.getId().equals(id)) {
+        return true;
+      }
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	@Override
-	public void delete(Long aLong) {
-		map.remove(aLong);
-	}
+  @Override
+  public void delete(Long aLong) {
+    map.remove(aLong);
+  }
 }
