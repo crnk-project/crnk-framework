0730eb5beef6750d14c9e54eb30eb900a01aa29d
MacLeod Broad <macleod.broad@workiva.com>
2019-10-11 15:51:01 -0400

    Removed unused Operation Merge function

--- crnk-gen/crnk-gen-openapi/src/main/java/io/crnk/gen/openapi/internal/annotations/OASAnnotations.java
+++ crnk-gen/crnk-gen-openapi/src/main/java/io/crnk/gen/openapi/internal/annotations/OASAnnotations.java
@@ -5,7 +5,6 @@ import io.crnk.gen.openapi.internal.OASMergeUtil;
 import io.crnk.meta.model.MetaAttribute;
 import io.crnk.meta.model.resource.MetaResource;
 import io.swagger.v3.core.converter.ModelConverters;
-import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.media.Schema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,21 +31,5 @@ public class OASAnnotations {
     Schema derivedSchema = model.get(words[words.length - 1]);
     derivedSchema = (Schema) derivedSchema.getProperties().get(metaAttribute.getUnderlyingName());
     return OASMergeUtil.mergeSchema(schema, derivedSchema);
-  }
-
-  public static Operation applyFromModel(Operation operation, MetaResource metaResource, MetaAttribute metaAttribute) {
-    Map<String, Schema> model;
-
-    try {
-      model = ModelConverters.getInstance().read(metaResource.getImplementationType());
-    } catch (Exception e) {
-      LOGGER.error("Unable to parse model" + metaResource.getName(), e);
-      return operation;
-    }
-
-//    String[] words = metaResource.getImplementationClassName().split(Pattern.quote("."));
-//    Schema derivedSchema = model.get(words[words.length - 1]);
-//    derivedSchema = (Schema) derivedSchema.getProperties().get(metaAttribute.getUnderlyingName());
-    return OASMergeUtil.mergeOperations(operation, operation);
   }
 }
