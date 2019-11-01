6c47fe548946e037ed47d2116e3bebb743d63833
Remo <remo.meier@adnovum.ch>
2019-11-01 09:43:46 +0100

    security: duplicate fix for role repository

--- crnk-security/src/main/java/io/crnk/security/repository/RoleRepository.java
+++ crnk-security/src/main/java/io/crnk/security/repository/RoleRepository.java
@@ -1,13 +1,13 @@
 package io.crnk.security.repository;
 
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.ResourceRepositoryBase;
 import io.crnk.core.resource.list.ResourceList;
 import io.crnk.security.SecurityModule;
-
-import java.util.Set;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 public class RoleRepository extends ResourceRepositoryBase<Role, UUID> {
 
@@ -22,8 +22,10 @@ public class RoleRepository extends ResourceRepositoryBase<Role, UUID> {
     public ResourceList<Role> findAll(QuerySpec querySpec) {
         Set<Role> resources = module.getConfig().getRules()
                 .stream()
-                .filter(it -> it.getRole() != null)
-                .map(it -> new Role(it.getRole()))
+				.map(it -> it.getRole())
+				.filter(roleName -> roleName != null)
+				.distinct()
+                .map(roleName -> new Role(roleName))
                 .collect(Collectors.toSet());
         return querySpec.apply(resources);
     }
