805bfa6e4e32dc30dc68b926b9075101a512cf28
Gianin Basler <gianin.basler@adnovum.ch>
2020-01-24 13:31:35 +0100

    Merge crnk-framework master

--- crnk-client/src/test/java/io/crnk/client/module/ClientModuleFactoryTest.java
+++ crnk-client/src/test/java/io/crnk/client/module/ClientModuleFactoryTest.java
@@ -1,7 +1,5 @@
 package io.crnk.client.module;
 
-import java.util.List;
-
 import io.crnk.client.CrnkClient;
 import io.crnk.core.engine.information.resource.ResourceInformationProviderModule;
 import io.crnk.core.engine.internal.jackson.JacksonModule;
@@ -9,6 +7,8 @@ import io.crnk.core.module.Module;
 import io.crnk.test.mock.ClientTestModule;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class ClientModuleFactoryTest {
 
@@ -33,17 +33,5 @@ public class ClientModuleFactoryTest {
 	public void shouldDiscoverObjectLinkModules() {
 		CrnkClient client = new CrnkClient("http://something", CrnkClient.ClientType.OBJECT_LINKS);
 		checkSharedAssertions(client);
-	}
-
-	@Test
-	public void shouldDiscoverObjectLinkModules() {
-		CrnkClient client = new CrnkClient("http://something", CrnkClient.ClientType.OBJECT_LINKS);
-		client.findModules();
-		List<Module> modules = client.getModuleRegistry().getModules();
-
-		Assert.assertEquals(3, modules.size());
-		Assert.assertEquals(ClientModule.class, modules.get(0).getClass());
-		Assert.assertEquals(JacksonModule.class, modules.get(1).getClass());
-		Assert.assertEquals(TestModule.class, modules.get(2).getClass());
 	}
 }
