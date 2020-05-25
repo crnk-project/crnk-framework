4e0940ef399045db6285aa972d2d24e6a5d21438
Remo Meier <remo.meier@adnovum.ch>
2020-05-25 11:17:08 +0200

    accept any JpaEntityRepositoryBase by JpaFacetProvider #413
    useful since JpaEntityRepositoryBase is considered the new default for JPA-based repository implementations.

--- crnk-data/crnk-data-jpa/src/main/java/io/crnk/data/jpa/internal/facet/JpaFacetProvider.java
+++ crnk-data/crnk-data-jpa/src/main/java/io/crnk/data/jpa/internal/facet/JpaFacetProvider.java
@@ -1,5 +1,14 @@
 package io.crnk.data.jpa.internal.facet;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+
 import io.crnk.core.engine.parser.TypeParser;
 import io.crnk.core.engine.registry.RegistryEntry;
 import io.crnk.core.queryspec.FilterOperator;
@@ -12,7 +21,7 @@ import io.crnk.data.facet.FacetValue;
 import io.crnk.data.facet.config.BasicFacetInformation;
 import io.crnk.data.facet.config.FacetInformation;
 import io.crnk.data.facet.provider.FacetProviderBase;
-import io.crnk.data.jpa.JpaEntityRepository;
+import io.crnk.data.jpa.JpaEntityRepositoryBase;
 import io.crnk.data.jpa.internal.JpaRepositoryUtils;
 import io.crnk.data.jpa.internal.JpaRequestContext;
 import io.crnk.data.jpa.internal.query.QueryBuilder;
@@ -20,94 +29,86 @@ import io.crnk.data.jpa.internal.query.backend.criteria.JpaCriteriaQueryBackend;
 import io.crnk.data.jpa.internal.query.backend.criteria.JpaCriteriaQueryImpl;
 import io.crnk.data.jpa.query.JpaQueryFactory;
 
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class JpaFacetProvider extends FacetProviderBase implements Prioritizable {
 
 
-    @Override
-    public boolean accepts(RegistryEntry entry) {
-        Object resourceRepository = entry.getResourceRepository().getImplementation();
-        return unwrap(resourceRepository) instanceof JpaEntityRepository;
-    }
+	@Override
+	public boolean accepts(RegistryEntry entry) {
+		Object resourceRepository = entry.getResourceRepository().getImplementation();
+		return unwrap(resourceRepository) instanceof JpaEntityRepositoryBase;
+	}
 
-    private Object unwrap(Object resourceRepository) {
-        // TODO ResourceRepositoryDecorator support
-        while (resourceRepository instanceof Wrapper) {
-            resourceRepository = ((Wrapper) resourceRepository).getWrappedObject();
-        }
-        return resourceRepository;
-    }
+	private Object unwrap(Object resourceRepository) {
+		// TODO ResourceRepositoryDecorator support
+		while (resourceRepository instanceof Wrapper) {
+			resourceRepository = ((Wrapper) resourceRepository).getWrappedObject();
+		}
+		return resourceRepository;
+	}
 
-    @Override
-    public List<FacetValue> findValues(FacetInformation facetInformation, QuerySpec querySpec) {
-        if (facetInformation instanceof BasicFacetInformation) {
-            String resourceType = facetInformation.getResource().getResourceType();
-            ResourceRepository repository = (ResourceRepository) context.getEntry(resourceType).getResourceRepository().getImplementation();
-            JpaEntityRepository entityRepository = (JpaEntityRepository) unwrap(repository);
-            BasicFacetInformation basicFacetInformation = (BasicFacetInformation) facetInformation;
+	@Override
+	public List<FacetValue> findValues(FacetInformation facetInformation, QuerySpec querySpec) {
+		if (facetInformation instanceof BasicFacetInformation) {
+			String resourceType = facetInformation.getResource().getResourceType();
+			ResourceRepository repository = (ResourceRepository) context.getEntry(resourceType).getResourceRepository().getImplementation();
+			JpaEntityRepositoryBase entityRepository = (JpaEntityRepositoryBase) unwrap(repository);
+			BasicFacetInformation basicFacetInformation = (BasicFacetInformation) facetInformation;
 
-            PathSpec path = basicFacetInformation.getPath();
+			PathSpec path = basicFacetInformation.getPath();
 
-            TypeParser typeParser = context.getTypeParser();
-            Map<Object, FacetValue> facetValueMap = new HashMap<>();
+			TypeParser typeParser = context.getTypeParser();
+			Map<Object, FacetValue> facetValueMap = new HashMap<>();
 
-            // setup query
-            JpaQueryFactory queryFactory = entityRepository.getQueryFactory();
-            Class entityClass = entityRepository.getEntityClass();
-            JpaCriteriaQueryImpl query = (JpaCriteriaQueryImpl) queryFactory.query(entityClass);
-            query.setPrivateData(new JpaRequestContext(entityRepository, querySpec));
-            QuerySpec filteredQuerySpec = JpaRepositoryUtils.filterQuerySpec(entityRepository.getRepositoryConfig(), entityRepository, querySpec);
-            JpaRepositoryUtils.prepareQuery(query, filteredQuerySpec, Collections.emptySet());
-
-
-            // create query
-            JpaCriteriaQueryBackend backend = query.newBackend();
-            CriteriaQuery criteriaQuery = backend.getCriteriaQuery();
-            QueryBuilder executorFactory = new QueryBuilder(query, backend);
-            executorFactory.applyFilterSpec();
-
-            // perform grouping
-            Expression expression = (Expression) executorFactory.getExpression(path);
-            criteriaQuery.groupBy(expression);
-
-            // perform selection
-            CriteriaBuilder criteriaBuilder = backend.getCriteriaBuilder();
-            Expression<Long> countExpr = criteriaBuilder.count(expression);
-            criteriaQuery.multiselect(expression, countExpr);
+			// setup query
+			JpaQueryFactory queryFactory = entityRepository.getQueryFactory();
+			Class entityClass = entityRepository.getEntityClass();
+			JpaCriteriaQueryImpl query = (JpaCriteriaQueryImpl) queryFactory.query(entityClass);
+			query.setPrivateData(new JpaRequestContext(entityRepository, querySpec));
+			QuerySpec filteredQuerySpec = JpaRepositoryUtils.filterQuerySpec(entityRepository.getRepositoryConfig(), entityRepository, querySpec);
+			JpaRepositoryUtils.prepareQuery(query, filteredQuerySpec, Collections.emptySet());
 
 
-            TypedQuery typedQuery = queryFactory.getEntityManager().createQuery(criteriaQuery);
-            List<Object[]> resultList = typedQuery.getResultList();
-            for (Object[] result : resultList) {
-                Object value = result[0];
-                Long count = (Long) result[1];
-                String label = typeParser.toString(value);
+			// create query
+			JpaCriteriaQueryBackend backend = query.newBackend();
+			CriteriaQuery criteriaQuery = backend.getCriteriaQuery();
+			QueryBuilder executorFactory = new QueryBuilder(query, backend);
+			executorFactory.applyFilterSpec();
 
-                FacetValue facetValue = new FacetValue();
-                facetValue.setValue(value);
-                facetValue.setLabel(label);
-                facetValue.setCount(count);
-                facetValue.setFilterSpec(path.filter(FilterOperator.EQ, value));
+			// perform grouping
+			Expression expression = (Expression) executorFactory.getExpression(path);
+			criteriaQuery.groupBy(expression);
 
-                facetValueMap.put(value, facetValue);
-            }
-            return toList(facetValueMap);
-        } else {
-            throw new UnsupportedOperationException("unknown facet type: " + facetInformation);
-        }
+			// perform selection
+			CriteriaBuilder criteriaBuilder = backend.getCriteriaBuilder();
+			Expression<Long> countExpr = criteriaBuilder.count(expression);
+			criteriaQuery.multiselect(expression, countExpr);
 
-    }
 
-    @Override
-    public int getPriority() {
-        return 500;
-    }
+			TypedQuery typedQuery = queryFactory.getEntityManager().createQuery(criteriaQuery);
+			List<Object[]> resultList = typedQuery.getResultList();
+			for (Object[] result : resultList) {
+				Object value = result[0];
+				Long count = (Long) result[1];
+				String label = typeParser.toString(value);
+
+				FacetValue facetValue = new FacetValue();
+				facetValue.setValue(value);
+				facetValue.setLabel(label);
+				facetValue.setCount(count);
+				facetValue.setFilterSpec(path.filter(FilterOperator.EQ, value));
+
+				facetValueMap.put(value, facetValue);
+			}
+			return toList(facetValueMap);
+		}
+		else {
+			throw new UnsupportedOperationException("unknown facet type: " + facetInformation);
+		}
+
+	}
+
+	@Override
+	public int getPriority() {
+		return 500;
+	}
 }
