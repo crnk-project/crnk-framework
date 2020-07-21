012ba78d4d9dbe435b6666ca40f1304d530d0184
Remo Meier <remo.meier@adnovum.ch>
2020-07-21 08:25:50 +0200

    Allow standalone use of InMemoryResourceRepository #768
    currently in must be managed by a CrnkBoot/ResourceRegistry in order to retrieve the ID field. As a heuristic it should fallback to a manual discovery of that field to ease its use.

--- crnk-core/src/main/java/io/crnk/core/engine/information/bean/BeanInformation.java
+++ crnk-core/src/main/java/io/crnk/core/engine/information/bean/BeanInformation.java
@@ -5,112 +5,112 @@ import io.crnk.core.engine.internal.utils.ClassUtils;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class BeanInformation {
 
-    private final Class implementationClass;
+	private final Class implementationClass;
 
-    private Map<String, BeanAttributeInformation> attributeMap = new HashMap<>();
+	private Map<String, BeanAttributeInformation> attributeMap = new HashMap<>();
 
-    private Map<String, BeanAttributeInformation> jsonAttributeMap = new HashMap<>();
+	private Map<String, BeanAttributeInformation> jsonAttributeMap = new HashMap<>();
 
-    private List<String> attributeNames = new ArrayList<>();
+	private List<String> attributeNames = new ArrayList<>();
 
-    private BeanInformation superType;
+	private BeanInformation superType;
 
-    private List<BeanInformation> implementedInterfaces = new ArrayList<>();
+	private List<BeanInformation> implementedInterfaces = new ArrayList<>();
 
-    private BeanInformation(Class implementationClass) {
-        this.implementationClass = implementationClass;
+	private BeanInformation(Class implementationClass) {
+		this.implementationClass = implementationClass;
 
-        List<Field> fields = ClassUtils.getClassFields(implementationClass);
-        List<Method> getters = ClassUtils.getClassGetters(implementationClass);
+		List<Field> fields = ClassUtils.getClassFields(implementationClass);
+		List<Method> getters = ClassUtils.getClassGetters(implementationClass);
 
-        for (Field field : fields) {
-            if (!Modifier.isStatic(field.getModifiers())) {
-                BeanAttributeInformation attrDesc = new BeanAttributeInformation(this, field);
-                attributeMap.put(field.getName(), attrDesc);
-                attributeNames.add(field.getName());
-            }
-        }
+		for (Field field : fields) {
+			if (!Modifier.isStatic(field.getModifiers())) {
+				BeanAttributeInformation attrDesc = new BeanAttributeInformation(this, field);
+				attributeMap.put(field.getName(), attrDesc);
+				attributeNames.add(field.getName());
+			}
+		}
 
-        for (Method getter : getters) {
-            if (!Modifier.isStatic(getter.getModifiers())) {
-                String name = ClassUtils.getGetterFieldName(getter);
-                BeanAttributeInformation attrDesc = attributeMap.get(name);
-                if (attrDesc == null) {
-                    attrDesc = new BeanAttributeInformation(this, getter, name);
-                    attributeMap.put(name, attrDesc);
-                    attributeNames.add(name);
-                } else {
-                    attrDesc.setGetter(getter);
-                }
-            }
-        }
+		for (Method getter : getters) {
+			if (!Modifier.isStatic(getter.getModifiers())) {
+				String name = ClassUtils.getGetterFieldName(getter);
+				BeanAttributeInformation attrDesc = attributeMap.get(name);
+				if (attrDesc == null) {
+					attrDesc = new BeanAttributeInformation(this, getter, name);
+					attributeMap.put(name, attrDesc);
+					attributeNames.add(name);
+				} else {
+					attrDesc.setGetter(getter);
+				}
+			}
+		}
 
-        Iterator<String> iterator = attributeNames.iterator();
-        while (iterator.hasNext()) {
-            String name = iterator.next();
-            BeanAttributeInformation attributeInformation = attributeMap.get(name);
-            Field field = attributeInformation.getField();
-            Method getter = attributeInformation.getGetter();
-            if ((field == null || !Modifier.isPublic(field.getModifiers())) && (getter == null || !Modifier
-                    .isPublic(getter.getModifiers()))) {
-                // no public accessor
-                iterator.remove();
-                attributeMap.remove(name);
-            }
-        }
+		Iterator<String> iterator = attributeNames.iterator();
+		while (iterator.hasNext()) {
+			String name = iterator.next();
+			BeanAttributeInformation attributeInformation = attributeMap.get(name);
+			Field field = attributeInformation.getField();
+			Method getter = attributeInformation.getGetter();
+			if ((field == null || !Modifier.isPublic(field.getModifiers())) && (getter == null || !Modifier
+					.isPublic(getter.getModifiers()))) {
+				// no public accessor
+				iterator.remove();
+				attributeMap.remove(name);
+			}
+		}
 
-        for (BeanAttributeInformation attrDesc : attributeMap.values()) {
-            String name = attrDesc.getName();
-            Class<?> attrType = attrDesc.getImplementationClass();
-            attrDesc.setSetter(ClassUtils.findSetter(implementationClass, name, attrType));
-            jsonAttributeMap.put(attrDesc.getJsonName(), attrDesc);
-        }
+		for (BeanAttributeInformation attrDesc : attributeMap.values()) {
+			String name = attrDesc.getName();
+			Class<?> attrType = attrDesc.getImplementationClass();
+			attrDesc.setSetter(ClassUtils.findSetter(implementationClass, name, attrType));
+			jsonAttributeMap.put(attrDesc.getJsonName(), attrDesc);
+		}
 
-        if (implementationClass.getSuperclass() != null && implementationClass.getSuperclass() != Object.class) {
-            superType = new BeanInformation(implementationClass.getSuperclass());
-        }
+		if (implementationClass.getSuperclass() != null && implementationClass.getSuperclass() != Object.class) {
+			superType = new BeanInformation(implementationClass.getSuperclass());
+		}
 
-        for (Class<?> interfaceClass : implementationClass.getInterfaces()) {
-            implementedInterfaces.add(new BeanInformation(interfaceClass));
-        }
-    }
+		for (Class<?> interfaceClass : implementationClass.getInterfaces()) {
+			implementedInterfaces.add(new BeanInformation(interfaceClass));
+		}
+	}
 
-    public BeanInformation getSuperType() {
-        return superType;
-    }
+	public BeanInformation getSuperType() {
+		return superType;
+	}
 
-    public List<BeanInformation> getImplementedInterfaces() {
-        return implementedInterfaces;
-    }
+	public List<BeanInformation> getImplementedInterfaces() {
+		return implementedInterfaces;
+	}
 
-    public BeanAttributeInformation getAttribute(String name) {
-        return attributeMap.get(name);
-    }
+	public BeanAttributeInformation getAttribute(String name) {
+		return attributeMap.get(name);
+	}
 
-    public BeanAttributeInformation getAttributeByJsonName(String jsonName) {
-        return jsonAttributeMap.get(jsonName);
-    }
+	public Collection<BeanAttributeInformation> getAttributes() {
+		return attributeMap.values();
+	}
 
-    public List<String> getAttributeNames() {
-        return attributeNames;
-    }
+	public BeanAttributeInformation getAttributeByJsonName(String jsonName) {
+		return jsonAttributeMap.get(jsonName);
+	}
 
-    public Class getImplementationClass() {
-        return implementationClass;
-    }
+	public List<String> getAttributeNames() {
+		return attributeNames;
+	}
 
-    private static final ConcurrentHashMap<Class, BeanInformation> cache = new ConcurrentHashMap<>();
+	public Class getImplementationClass() {
+		return implementationClass;
+	}
 
-    public static BeanInformation get(Class<?> clazz) {
-        return cache.computeIfAbsent(clazz, BeanInformation::new);
-    }
+	private static final ConcurrentHashMap<Class, BeanInformation> cache = new ConcurrentHashMap<>();
+
+	public static BeanInformation get(Class<?> clazz) {
+		return cache.computeIfAbsent(clazz, BeanInformation::new);
+	}
 }
--- crnk-core/src/main/java/io/crnk/core/repository/InMemoryResourceRepository.java
+++ crnk-core/src/main/java/io/crnk/core/repository/InMemoryResourceRepository.java
@@ -1,13 +1,25 @@
 package io.crnk.core.repository;
 
-import io.crnk.core.engine.information.resource.ResourceField;
+import io.crnk.core.boot.CrnkBoot;
+import io.crnk.core.engine.information.bean.BeanAttributeInformation;
+import io.crnk.core.engine.information.bean.BeanInformation;
+import io.crnk.core.engine.information.resource.*;
+import io.crnk.core.engine.internal.information.resource.DefaultResourceFieldInformationProvider;
+import io.crnk.core.engine.internal.information.resource.DefaultResourceInformationProvider;
+import io.crnk.core.engine.internal.information.resource.ReflectionFieldAccessor;
+import io.crnk.core.engine.internal.jackson.JacksonResourceFieldInformationProvider;
 import io.crnk.core.engine.internal.utils.PreconditionUtil;
+import io.crnk.core.engine.properties.NullPropertiesProvider;
 import io.crnk.core.engine.registry.RegistryEntry;
 import io.crnk.core.engine.registry.ResourceRegistry;
 import io.crnk.core.queryspec.QuerySpec;
+import io.crnk.core.queryspec.pagingspec.OffsetLimitPagingBehavior;
+import io.crnk.core.resource.annotations.JsonApiId;
 import io.crnk.core.resource.list.ResourceList;
 
+import java.beans.BeanInfo;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -15,45 +27,54 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class InMemoryResourceRepository<T, I> extends ResourceRepositoryBase<T, I> {
 
-    protected Map<I, T> resources = new ConcurrentHashMap<>();
+	protected Map<I, T> resources = new ConcurrentHashMap<>();
 
-    private ResourceRegistry resourceRegistry;
+	private ResourceRegistry resourceRegistry;
 
-    public InMemoryResourceRepository(Class<T> resourceClass) {
-        super(resourceClass);
-    }
+	private ResourceFieldAccessor idField;
 
-    public Map<I, T> getMap() {
-        return resources;
-    }
+	public InMemoryResourceRepository(Class<T> resourceClass) {
+		super(resourceClass);
+	}
 
-    public void clear() {
-        resources.clear();
-    }
+	public Map<I, T> getMap() {
+		return resources;
+	}
 
-    @Override
-    public ResourceList<T> findAll(QuerySpec querySpec) {
-        return querySpec.apply(resources.values());
-    }
+	public void clear() {
+		resources.clear();
+	}
 
-    @Override
-    public <S extends T> S save(S entity) {
-        RegistryEntry entry = resourceRegistry.findEntry(getResourceClass());
-        ResourceField idField = entry.getResourceInformation().getIdField();
-        I id = (I) idField.getAccessor().getValue(entity);
-        PreconditionUtil.verify(id != null, "resource %s must have an identifier", resources);
-        resources.put(id, entity);
-        return entity;
-    }
+	@Override
+	public ResourceList<T> findAll(QuerySpec querySpec) {
+		return querySpec.apply(resources.values());
+	}
 
-    @Override
-    public void delete(I id) {
-        resources.remove(id);
-    }
+	@Override
+	public <S extends T> S save(S entity) {
+		if (idField == null && resourceRegistry != null) {
+			RegistryEntry entry = resourceRegistry.findEntry(getResourceClass());
+			idField = entry.getResourceInformation().getIdField().getAccessor();
+		} else if (idField == null) {
+			BeanInformation beanInformation = BeanInformation.get(getResourceClass());
+			Optional<BeanAttributeInformation> idAttr = beanInformation.getAttributes().stream().filter(it -> it.getAnnotation(JsonApiId.class) != null).findFirst();
+			PreconditionUtil.verify(idAttr.isPresent(), "no @JsonApiId attribute found on %s", getResourceClass());
+			idField = new ReflectionFieldAccessor(getResourceClass(), idAttr.get().getName(), idAttr.get().getImplementationClass());
+		}
+		I id = (I) idField.getValue(entity);
+		PreconditionUtil.verify(id != null, "resource %s must have an identifier", resources);
+		resources.put(id, entity);
+		return entity;
+	}
 
-    @Override
-    public void setResourceRegistry(ResourceRegistry resourceRegistry) {
-        this.resourceRegistry = resourceRegistry;
-        super.setResourceRegistry(resourceRegistry);
-    }
+	@Override
+	public void delete(I id) {
+		resources.remove(id);
+	}
+
+	@Override
+	public void setResourceRegistry(ResourceRegistry resourceRegistry) {
+		this.resourceRegistry = resourceRegistry;
+		super.setResourceRegistry(resourceRegistry);
+	}
 }
