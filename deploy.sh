4d86e207549ef3d997cdc06d90754a8a95e2818c
Remo Meier <remo.meier@adnovum.ch>
2020-08-31 07:41:47 +0200

    error handling added to publishing

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set - e
 if [ "$TRAVIS_BRANCH" == "stable" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
   ./gradlew publish promote --max-workers=1 --no-parallel -Pstable=true
 elif [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
